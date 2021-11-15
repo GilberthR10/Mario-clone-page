4e8a7676c3f3bc43097534c5ce802f98082fe898
Gilberth Renteria <gilberthr10@gmail.com>
2021-11-14 19:39:24 -0500

    firts commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/img/wave-white.png')",
+        'hero-pattern-dots': "url('/assets/img/pattern-white-dots.png')",
+        'pattern-characters-red': "url('/assets/img/pattern-characters-red.png')",
+        'character-mario': "url('/assets/img/character-l.png')",
+        'wave-pink': "url('/assets/img/wave-pink.png')",
+        'yellow-dots': "url('/assets/img/pattern-yellow-dots.png')",
+      }
+    },
+  },
+  plugins: [],
+}
